3d4376f01cad11bbf1c16a07601ec1b47c56a975
Abdelrahman Shawky <abdelrahman.a.shawky@gmail.com>
2023-03-04 17:34:10 +0200

    login

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,10 +1,22 @@
 const User = require('../models/User');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+
 
 // handle errors
 const handleErrors = (err) => {
     console.log(err.message, err.code);
     let errors = { email:'', password: ''};
+
+    // incorrect email
+    if (err.message === 'Incorrect Email') {
+        error.email = 'that email is not registered';
+    }
+
+    // incorrect password
+    if (err.message === 'Incorrect Password') {
+        error.password = 'that password is incorrect';
+    }
 
     // duplicate error code
     if (err.code === 11000) {
@@ -55,6 +67,15 @@ module.exports.signup_post = async (req, res) => {
 
 module.exports.login_post = async (req, res) => {
     const {email, password} = req.body;
-    console.log(email, password);
-    res.send('user login');
+    try {
+        const user = await User.login(email, password);
+        const token = createToken(user._id);
+        res.cookie('jwt', token, {httpOnly: true, maxAge: maxAge*1000});
+        res.status(200).json({user: user._id});
+    }
+    catch (err) {
+        console.log(err);
+        const errors = handleErrors(err);
+        res.status(400).json({errors});
+    }
 }
