2be63a0b9890532fb7764e89ec2011a34d12cec3
Abdelrahman Shawky <abdelrahman.a.shawky@gmail.com>
2023-03-04 16:21:24 +0200

    added jwt

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 const User = require('../models/User');
+const jwt = require('jsonwebtoken');
 
 // handle errors
 const handleErrors = (err) => {
@@ -20,6 +21,15 @@ const handleErrors = (err) => {
     return errors;
 }
 
+// 3 days in seconds
+const maxAge = 3 * 24 * 60 *60;
+
+const createToken = (id) => {
+    return jwt.sign({ id }, 'secret key !', {
+        expiresIn: maxAge 
+    });
+}
+
 module.exports.signup_get = (req, res) => {
     res.render('signup');
 }
@@ -32,7 +42,9 @@ module.exports.signup_post = async (req, res) => {
     const {email, password} = req.body;
     try {
         const user = await User.create({email, password});
-        res.status(201).json(user);
+        const token = createToken(user._id);
+        res.cookie('jwt', token, {httpOnly: true, maxAge: maxAge*1000});
+        res.status(201).json({user: user._id});
     }
     catch (err) {
         console.log(err);
