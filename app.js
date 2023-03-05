49566b6deb4ed40bd59dd4ef60efcf8492b01abc
Abdelrahman Shawky <abdelrahman.a.shawky@gmail.com>
2023-03-05 06:10:15 +0200

    updated env

--- app.js
+++ app.js
@@ -18,9 +18,10 @@ app.set('view engine', 'ejs');
 
 // database connection
 const dbURI = process.env.dbURI;
+const port = process.env.PORT || 3000;
 
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true})
-  .then((result) => app.listen(3000))
+  .then((result) => app.listen(port))
   .catch((err) => console.log(err));
 
 // routes
